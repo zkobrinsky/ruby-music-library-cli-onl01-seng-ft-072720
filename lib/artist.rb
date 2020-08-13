65e79eb38db03e38373b4773331e9e228f68d9b1
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 20:54:39 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    inst = self.new(name).tap {|inst| inst.save}
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    unless song.artist != nil
+      song.artist = self
+      @songs << song
+    end
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+
+  attr_accessor :name
+  attr_reader :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    inst = self.new(name).tap {|inst| inst.save}
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+end
