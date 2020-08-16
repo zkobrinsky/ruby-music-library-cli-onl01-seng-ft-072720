63324b2aa78e72bbe9ae3643846da4d36094c8ca
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-16 16:58:21 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController
 
   attr_accessor :path
-  @input = ""
+
   @library = ""
 
   def initialize(path = './db/mp3s')
@@ -11,7 +11,8 @@ class MusicLibraryController
   end
 
   def call
-    until @input == "exit"
+    input = ""
+    until input == "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -21,7 +22,22 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      @input = gets.chomp
+      input = gets.chomp
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
   end
 
@@ -41,17 +57,33 @@ class MusicLibraryController
   end
 
   def list_songs_by_artist
-    # binding.pry
     puts "Please enter the name of an artist:"
     input = gets.chomp
 
-
-
-    # artist_songs = Song.all.select{|s| s.artist.name == input}
-    #
-  end
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort_by{|s| s.name}.each.with_index(1){|s, i| puts "#{i}. #{s.name} - #{s.genre.name}"}
+    end
   end
 
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.chomp
+
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort_by{|s| s.name}.each.with_index(1){|s, i| puts "#{i}. #{s.artist.name} - #{s.name}"}
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.chomp.to_i
+    songs = Song.all.sort_by{|s| s.name}
+
+    # unless input >= songs.length
+    unless songs[input-1] == nil || input < 1 || input >= songs.length
+      puts "Playing #{songs[input-1].name} by #{songs[input-1].artist.name}"
+    end
+  end
 
 
 end
