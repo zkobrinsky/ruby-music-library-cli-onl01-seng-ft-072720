c3b7a9dda739a15868ddf71edb1b4a00c3041be5
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-16 16:56:00 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController
 
   attr_accessor :path
-  @input = ""
+  
   @library = ""
 
   def initialize(path = './db/mp3s')
@@ -11,7 +11,8 @@ class MusicLibraryController
   end
 
   def call
-    until @input == "exit"
+    input = ""
+    until input == "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -21,7 +22,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      @input = gets.chomp
+      input = gets.chomp
     end
   end
 
